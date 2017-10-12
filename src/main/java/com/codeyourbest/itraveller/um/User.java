e4b4c300048c7d870cab197723ffcdfe556bcfd2
klaskoski <konrad.laskoski@e-podroznik.pl>
2017-10-12 14:43:40 +0200

    added Lombok and folder for requests

--- src/main/java/com/codeyourbest/itraveller/um/User.java
+++ src/main/java/com/codeyourbest/itraveller/um/User.java
@@ -1,7 +1,11 @@
 package com.codeyourbest.itraveller.um;
 
+import lombok.Data;
+import lombok.NonNull;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "Users")
 public class User {
@@ -13,67 +17,23 @@ public class User {
 
     @Column(nullable = false)
     private String name;
-
     @Column(nullable = false)
     private String surname;
-
     @Column(nullable = false)
     private String email;
-
     @Column(nullable = false)
     private String phone;
 
-    public User(String name, String surname, String email, String phone) {
+    private User (){}
+
+    public User(@NonNull String name,
+                @NonNull String surname,
+                @NonNull String email,
+                @NonNull String phone) {
+
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.phone = phone;
-    }
-
-    public User (){
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                '}';
     }
 }
